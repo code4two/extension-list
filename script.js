7c639b0eaa54bc77512bce9e70cd93184cefb86d
Peter Adepoju <peteradepoju22@gmail.com>
2025-08-24 18:58:52 +0100

    added JavaScript file

---
+++ script.js
@@ -0,0 +1,16 @@
+let mopo = document.querySelector(".nike");
+
+mopo.addEventListener("click", () => {
+  console.log("You are beautiful!!!!");
+
+  let cardBg = document.querySelector(".fiber");
+  let bodyBg = document.querySelector("body");
+  let extensionBar = document.querySelector(".stone");
+
+  function LightTheme() {
+    console.log("I am trying to master JavaScript");
+    bodyBg.style.color = "blue";
+  }
+
+  LightTheme();
+});
