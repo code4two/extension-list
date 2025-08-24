ce319de19cf72601c10c2f428572d03b2a7237f5
Peter Adepoju <peteradepoju22@gmail.com>
2025-08-24 20:26:43 +0100

    updated file

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let mopo = document.querySelector(".nike");
 
+// classes with nodelist yet to be assigned tasks
+let cardBg = document.querySelectorAll(".fiber"); // Returns nodelist for all 12 fiber classes that represent the card
+let inactive = document.querySelectorAll(".bath1");
+
 mopo.addEventListener("click", () => {
   console.log("You are beautiful!!!!");
 
-  let cardBg = document.querySelector(".fiber");
   let bodyBg = document.querySelector("body");
   let extensionBar = document.querySelector(".stone");
+  let anchor = document.querySelector("a");
 
   function LightTheme() {
     console.log("I am trying to master JavaScript");
-    bodyBg.style.color = "blue";
+
+    bodyBg.style.color = "#000"; // Changes the body tag text color to black
+    bodyBg.style.backgroundColor = "#E3F2FD"; // Changes the body tag background color to lightblue
+    cardBg[3].style.backgroundColor = "#fff"; // Changes all card background color in the nodelist to white
+    cardBg[3].style.border = "none"; // Removes the card border
+    extensionBar.style.backgroundColor = "#fff"; // Changes the extension bar bg color to white
+    mopo.style.backgroundColor = "#D3D3D3"; // Changes the themebar bg color to white
+    anchor.style.color = "#000"; // Changes the anchor tag text color to black
   }
 
   LightTheme();
