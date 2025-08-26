65b1ddba43f7940793968e3534c77e68fb3eed78
Peter Adepoju <peteradepoju22@gmail.com>
2025-08-26 14:31:20 +0100

    lightTheme feature added to modal

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ mopo.addEventListener("click", () => {
   let bodyBg = document.querySelector("body");
   let extensionBar = document.querySelector(".stone");
   let inactive = document.querySelectorAll(".bath1"); // Returns nodelist for all 2 bath1 classes that represent the active and inactive pages
+  let modalCard = document.querySelector(".fighter"); // Modal card
+  let cancelButton = document.querySelector(".phiki"); // Returns cancel button
 
   function LightTheme() {
     console.log("2. LightTheme Function Clicked!!🤗");
@@ -16,6 +18,9 @@ mopo.addEventListener("click", () => {
     mopo.style.backgroundColor = "#D3D3D3"; // Changes the themebar bg color to white
     inactive[0].style.backgroundColor = "#fff"; // Changes the button bg colo to white
     inactive[1].style.backgroundColor = "#fff"; // Changes the button bg colo to white
+    modalCard.style.color = "#000"; // Changes the modalCard tag text color to black
+    modalCard.style.backgroundColor = "#fff"; // Changes the modalCard background color to white
+    cancelButton.style.backgroundColor = "#fff"; // Changes the cancelButton background color to white
 
     //USING FOR LOOP
     let cardBg = document.querySelectorAll(".fiber"); // Returns nodelist for all 12 fiber classes that represent the card
